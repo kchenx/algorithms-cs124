05445cc2b70f77eae63e60478aac7663cec7dda1
Kevin Chen <kevin.chen.krc@gmail.com>
2019-03-09 15:30:46 -0500

    Add new alg

---
+++ pset4/solution.py
@@ -0,0 +1,11 @@
+# Solves CS 124 programming problem in directory
+
+N, M, K = map(int, raw_input().split())
+
+for i in xrange(M):
+    p, q = map(int, raw_input().split())
+
+for i in xrange(Q):
+    t, a, b = map(int, raw_input().split())
+
+
