2dc6ecc232b8836fd5d81c65fbf7957c8dce8708
Kevin Chen <kevin.chen.krc@gmail.com>
2019-04-20 14:34:18 -0400

    Add new problem

---
+++ pset7/hashing.py
@@ -0,0 +1,11 @@
+# Solves CS 124 programming problem in directory
+
+N, B = map(int, raw_input().split())
+
+bug = raw_input()
+
+for i in xrange(B):
+    indicator = raw_input()
+    print indicator
+
+print bug
