c77eaedccb5b43fe2003d75f9ca6d9e955a43fdc
Kevin Chen <kevin.chen.krc@gmail.com>
2019-04-22 01:52:55 -0400

    Add new problem

---
+++ pset8/solution.py
@@ -0,0 +1,8 @@
+# Solves CS 124 programming problem in directory
+
+N, M = map(int, raw_input().split())
+
+for i in xrange(M):
+    a, b, c, d = map(int, raw_input().split())
+
+e = map(int, raw_input().split())
